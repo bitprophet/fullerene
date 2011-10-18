2c8c8e572e638fa44f3da352e252e27b55c96950
Jeff Forcier <jeff@bitprophet.org>
2011-10-18 00:41:12 +0000

    Remove runner code from main module

--- fullerene.py
+++ fullerene.py
@@ -124,11 +124,3 @@ def render():
         headers=response.headers,
     )
     return r
-
-
-#
-# Runner
-#
-
-if __name__ == "__main__":
-    app.run(host='localhost', port=8080, debug=True, extra_files=(CONFIG,))
